28ddcad2e77af82b9dc771b9f71916ae8dd3cd59
Putra Utama <putra.utamajaya@Hotmail.com>
2018-09-10 12:01:23 +0700

    update total harga 10.09.2018

--- src/component/cartPage.js
+++ src/component/cartPage.js
@@ -3,7 +3,8 @@ import { connect } from 'react-redux';
 
 class cartPage extends Component {
     state = { 
-        cart: []
+        cart: [],
+        totalHarga: 0
     }
 
     getCartData = () => {
@@ -20,7 +21,21 @@ class cartPage extends Component {
         this.getCartData();
     }
 
+    totalHarga = () => {
+        console.log(this.state.cart)
+        let total = 0;
+        this.state.cart.map( 
+            (item) => total = total + item.price
+        );
+        this.setState({totalHarga: total});
+    }
+    
+    componentDidMount() {
+        this.totalHarga();
+    }
+
     renderTableCart = () =>{
+        
         return this.state.cart.map( (item) => 
 
             <tr>
@@ -39,19 +54,22 @@ class cartPage extends Component {
                 <td data-th="Price">Rp. {item.price}</td>
 
                 <td data-th="Quantity">
-                    <input type="number" className="form-control text-center" value="1"/>
+                    <input type="number" className="form-control text-center" defaultValue="1"/>
                 </td>
-                <td data-th="Subtotal" className="text-center">1.99</td>
+                <td data-th="Subtotal" className="text-center">Rp. {item.price}</td>
                 <td className="actions" data-th="">
-                    <button className="btn btn-info btn-sm"><i className="fa fa-refresh"></i></button>
-                    <button className="btn btn-danger btn-sm"><i className="fa fa-trash-o"></i></button>								
+                    <button className="btn btn-info btn-sm"><i className="glyphicon glyphicon-refresh"></i></button>
+                    <button className="btn btn-danger btn-sm"><i className="glyphicon glyphicon-trash"></i></button>								
                 </td>
 
             </tr>
         ); 
+
     }
 
     render() {
+        console.log(this.state.cart );
+        console.log(this.state.totalHarga );
         return(
             <div className="container">
             <br/><br/><br/>
@@ -71,14 +89,11 @@ class cartPage extends Component {
                     </tbody>
 
                     <tfoot>
-                        <tr className="visible-xs">
-                            <td className="text-center"><strong>Total 1.99</strong></td>
-                        </tr>
                         <tr>
-                            <td><a href="#" className="btn btn-warning"><i className="fa fa-angle-left"></i> Continue Shopping</a></td>
+                            <td><a href="#" className="btn btn-warning"><i className="glyphicon glyphicon-chevron-left"></i> Continue Shopping</a></td>
                             <td colspan="2" className="hidden-xs"></td>
-                            <td className="hidden-xs text-center"><strong>Total $1.99</strong></td>
-                            <td><a href="#" className="btn btn-success btn-block">Checkout <i className="fa fa-angle-right"></i></a></td>
+                            <td className="hidden-xs text-center"><strong>Rp. {this.state.totalHarga}</strong></td>
+                            <td><a href="#" className="btn btn-success btn-block">Checkout <i className="glyphicon glyphicon-chevron-right"></i></a></td>
                         </tr>
                     </tfoot>
                 </table>
