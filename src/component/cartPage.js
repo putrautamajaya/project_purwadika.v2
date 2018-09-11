bb0af5db4ee4b938d1d13d2aa842ff6f63e61c1f
Putra Utama <putra.utamajaya@Hotmail.com>
2018-09-11 11:37:56 +0700

    update cart quantity

--- src/component/cartPage.js
+++ src/component/cartPage.js
@@ -4,7 +4,8 @@ import { connect } from 'react-redux';
 class cartPage extends Component {
     state = { 
         cart: [],
-        totalHarga: 0
+        totalHarga: 0,
+        itemQuantity: []
     }
 
     getCartData = () => {
@@ -29,14 +30,41 @@ class cartPage extends Component {
         );
         this.setState({totalHarga: total});
     }
+
+    //creating first quantity after opening the cartpage.
+    quantity = () => {
+        this.state.cart.map( 
+            (item) => this.state.itemQuantity.push({
+                id: item.id,
+                quantity: 1
+            })
+        )
+        console.log(this.state.itemQuantity)
+    }
     
+    //after 1st render do these
     componentDidMount() {
         this.totalHarga();
+        this.quantity();
+    }
+
+    //if + quantity click do this. adding quantity
+    onQuantityUpClick = (id) =>{
+        let quantityArr = this.state.itemQuantity;
+        for (let index in quantityArr){
+            if(quantityArr[index].id == id){
+                quantityArr[index].quantity += 1;
+                console.log('quantity di tambahkan 1')
+            }
+        }
+        this.setState({itemQuantity: quantityArr})
+        console.log(this.state.itemQuantity[0].quantity)
     }
 
     renderTableCart = () =>{
-        
-        return this.state.cart.map( (item) => 
+
+        if(this.state.itemQuantity[0] !== undefined){
+            return this.state.cart.map( (item) => 
 
             <tr>
         
@@ -54,17 +82,51 @@ class cartPage extends Component {
                 <td data-th="Price">Rp. {item.price}</td>
 
                 <td data-th="Quantity">
-                    <input type="number" className="form-control text-center" defaultValue="1"/>
+                    <input type="number" className="form-control text-center" value={this.state.itemQuantity[0].quantity} />
                 </td>
+
                 <td data-th="Subtotal" className="text-center">Rp. {item.price}</td>
+
                 <td className="actions" data-th="">
-                    <button className="btn btn-info btn-sm"><i className="glyphicon glyphicon-refresh"></i></button>
+                    <button className="btn btn-info btn-sm"><i className="glyphicon glyphicon-plus" onClick={() => this.onQuantityUpClick(item.id)}></i></button>
                     <button className="btn btn-danger btn-sm"><i className="glyphicon glyphicon-trash"></i></button>								
                 </td>
 
             </tr>
-        ); 
+            ); 
+        }
 
+        return this.state.cart.map( (item) => 
+
+        <tr>
+    
+            <td data-th="Product">
+                <div className="row">
+                    <div className="col-sm-2 hidden-xs"><img src={item.url} alt="..." className="img-responsive"/></div>
+                    <div className="col-sm-10">
+                        <h4 className="nomargin">{item.name}</h4>
+                        <p><b>Brand:</b> {item.brand}</p>
+                        <p><b>Type:</b> {item.type}</p>
+                    </div>
+                </div>
+            </td>
+
+            <td data-th="Price">Rp. {item.price}</td>
+
+            <td data-th="Quantity">
+                <input type="number" className="form-control text-center" defaultValue="1" />
+            </td>
+
+            <td data-th="Subtotal" className="text-center">Rp. {item.price}</td>
+
+            <td className="actions" data-th="">
+                <button className="btn btn-info btn-sm"><i className="glyphicon glyphicon-plus" onClick={() => this.onQuantityUpClick(item.id)}></i></button>
+                <button className="btn btn-danger btn-sm"><i className="glyphicon glyphicon-trash"></i></button>								
+            </td>
+
+        </tr>
+        ); 
+        
     }
 
     render() {
